f663677dc18885bd4f4093d9f5bc76135260c1c5
motimalu <red.back.spider.dev@gmail.com>
2025-06-08 16:06:00 +0900

    create amd mi300x 192gb config

---
+++ sd-scripts/SDXL/adafactor-02-192gb-v-pred/train_full_sdxl_vpred.sh
@@ -0,0 +1,12 @@
+source venv/bin/activate
+
+accelerate launch --num_cpu_threads_per_process=8 "./sdxl_train.py" \
+  --config_file="train_full_sdxl_vpred.toml" \
+  --dataset_config="train_dataset_sdxl.toml" \
+  --zero_terminal_snr \
+  --v_parameterization \
+  --learning_rate=1.4e-4 \
+  --learning_rate_te1=6e-5 \
+  --learning_rate_te2=6e-5\
+  --max_train_epochs=50 \
+  --save_every_n_epochs=10
