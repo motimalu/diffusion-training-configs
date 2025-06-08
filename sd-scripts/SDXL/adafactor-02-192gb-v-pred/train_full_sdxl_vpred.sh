45c5e9d0de37730e9caed6a902df9cd0a2272e09
motimalu <red.back.spider.dev@gmail.com>
2025-06-08 16:33:27 +0900

    add sdpa & and highvram flag for amd

--- sd-scripts/SDXL/adafactor-02-192gb-v-pred/train_full_sdxl_vpred.sh
+++ sd-scripts/SDXL/adafactor-02-192gb-v-pred/train_full_sdxl_vpred.sh
@@ -5,6 +5,8 @@ accelerate launch --num_cpu_threads_per_process=8 "./sdxl_train.py" \
   --dataset_config="train_dataset_sdxl.toml" \
   --zero_terminal_snr \
   --v_parameterization \
+  --sdpa \
+  --highvram \
   --learning_rate=1.4e-4 \
   --learning_rate_te1=6e-5 \
   --learning_rate_te2=6e-5\
