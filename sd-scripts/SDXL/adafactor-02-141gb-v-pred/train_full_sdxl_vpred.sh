b591c43ee3152fa0763d3d73066769e76de5920a
motimalu <red.back.spider.dev@gmail.com>
2025-06-08 17:45:53 +0900

    120 batch size

--- sd-scripts/SDXL/adafactor-02-141gb-v-pred/train_full_sdxl_vpred.sh
+++ sd-scripts/SDXL/adafactor-02-141gb-v-pred/train_full_sdxl_vpred.sh
@@ -6,8 +6,8 @@ accelerate launch --num_cpu_threads_per_process=8 "./sdxl_train.py" \
   --zero_terminal_snr \
   --v_parameterization \
   --highvram \
-  --learning_rate=2.1e-4 \
-  --learning_rate_te1=9e-5 \
-  --learning_rate_te2=9e-5\
+  --learning_rate=1.7e-4 \
+  --learning_rate_te1=7.2e-5 \
+  --learning_rate_te2=7.2e-5\
   --max_train_epochs=5 \
   --save_every_n_epochs=1
